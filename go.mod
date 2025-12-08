831542f5c2611b9f6fe90c204fe7dd5782e3365a
jack8096 <jackdeepakkamble@gmail.com>
2025-12-08 18:08:54 +0530

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/kissanjamgit/strmup
 
 go 1.25.1
+
+require resty.dev/v3 v3.0.0-beta.4
+
+require (
+	github.com/kissanjamgit/pornbox v0.0.0-20251208084658-b731a7683139
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"reflect"
+	"regexp"
+
+	"github.com/kissanjamgit/pornbox"
+	"resty.dev/v3"
+)
+
+type Strmup struct {
+	url string
+}
+
+func New(url string) Strmup {
+	return Strmup{url: url}
+}
+
+func (s *Strmup) Video() (cr pornbox.ContentResource, err error) {
+	id := regexp.MustCompile("[^/]+$").FindString(s.url)
+	return VideoByID(id)
+}
+
+func VideoByID(id string) (cr pornbox.ContentResource, err error) {
+	client := resty.New()
+	defer client.Close()
+	url_ := fmt.Sprintf("https://strmup.to/ajax/stream?filecode=%s", id)
+	resp, err := client.R().Get(url_)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var value any
+	err = json.Unmarshal(resp.Bytes(), &value)
+	if err != nil {
+		return
+	}
+	map_, ok := value.(map[string]any)
+	if !ok {
+		err = fmt.Errorf("value is't map, type: %s, value: %v", reflect.TypeOf(value).String(), value)
+		return
+	}
+	url, ok := map_["streaming_url"].(string)
+	if !ok {
+		err = fmt.Errorf("url.TypeOf != string value: %v", value)
+		return
+	}
+	title, ok := map_["title"].(string)
+	if !ok {
+		err = fmt.Errorf("title.TypeOf != string value: %v", value)
+		return
+	}
+
+	cr = pornbox.ContentResource{Name: func() string {
+		if title != "" {
+			return title
+		}
+		return fmt.Sprintf("Strmup Video %s", id)
+	}(), Url: url}
+	return
+}
+
+func main() {
+	id := flag.String("id", "", "")
+	url := flag.String("i", "", "")
+	flag.Parse()
+	if *id == "" && *url == "" {
+		fmt.Println(`*id == "" && *url == ""`)
+		os.Exit(1)
+	}
+	strmup := New(*url)
+	cr, err := strmup.Video()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(cr.Url)
+}
